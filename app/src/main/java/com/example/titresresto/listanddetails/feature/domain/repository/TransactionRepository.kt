a3e92e2b500ce553b420a51f3d22304d603117ee
Frederic Lemieux <frederic.lemieux14@gmail.com>
2021-12-03 13:20:30 +0400

    Define contract of the repository

---
+++ app/src/main/java/com/example/titresresto/listanddetails/feature/domain/repository/TransactionRepository.kt
@@ -0,0 +1,13 @@
+package com.example.titresresto.listanddetails.feature.domain.repository
+
+import com.example.base.tools.data.Resource
+import com.example.titresresto.listanddetails.feature.domain.model.Transaction
+import kotlinx.coroutines.flow.Flow
+
+interface TransactionRepository {
+
+    fun getTransactions() : Flow<Resource<List<Transaction>>>
+
+    suspend fun insertAll(transactions : List<Transaction>)
+
+}
