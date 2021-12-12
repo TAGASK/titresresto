dccdb29063c3c71e3d3aa1428eb1fa18a5a95dfb
Frederic Lemieux <frederic.lemieux14@gmail.com>
2021-12-12 22:42:56 +0400

    Rename variable

--- app/src/main/java/com/example/titresresto/listanddetails/feature/presenter/details/composable/DetailsHeader.kt
+++ app/src/main/java/com/example/titresresto/listanddetails/feature/presenter/details/composable/DetailsHeader.kt
@@ -2,25 +2,17 @@ package com.example.titresresto.listanddetails.feature.presenter.details.composa
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
 import coil.compose.rememberImagePainter
-import coil.transform.CircleCropTransformation
 import com.example.titresresto.listanddetails.feature.TitresRestoAppTheme
-import com.example.titresresto.listanddetails.feature.domain.model.Amount
 import com.example.titresresto.listanddetails.feature.presenter.Category
 import com.example.titresresto.listanddetails.feature.presenter.CategoryConstants
-import com.example.titresresto.listanddetails.feature.presenter.details.DetailsTransactionScreen
 
 @Composable
 fun DetailsHeader(
@@ -28,21 +20,21 @@ fun DetailsHeader(
     category :  String?,
     modifier : Modifier = Modifier
 ){
-    val pair = Category.selectIcon(category)
+    val categoryObject = Category.selectIcon(category)
     var colorF : ColorFilter? = null
     if(imageUrl == null) {
         ColorFilter.lighting(
-            multiply = pair.foreground,
-            add = pair.foreground
+            multiply = categoryObject.foreground,
+            add = categoryObject.foreground
         ).also { colorF = it }
     }
     Box(modifier = modifier.fillMaxWidth()
-        .background(pair.background)
+        .background(categoryObject.background)
         .height(224.dp),
         contentAlignment = Alignment.Center) {
         Image(
             painter = rememberImagePainter(
-                data = imageUrl ?: pair.idResource,
+                data = imageUrl ?: categoryObject.idResource,
             ),
             alpha = 1.0F,
             colorFilter = colorF,
